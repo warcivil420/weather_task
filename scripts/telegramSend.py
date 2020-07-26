31680ad38c014924265d92f345cc0d5640b7fcac
andrew <pixtergoood@gmail.com>
2020-07-26 07:17:08 +0500

    Update
    Небольшое увеличение производительности

--- scripts/telegramSend.py
+++ scripts/telegramSend.py
@@ -1,12 +1,11 @@
 import telebot
 import owmrequest as req
 bot = telebot.TeleBot('1331637578:AAGuUhHFAL9DiyDmGU8eh6CE7k_7XGnQVVs')
-date = []
 
-for i in req.weather_str:
-	date.append(i.split(" ")[0])
+date = [i.split(" ")[0] for i in req.weather_str]
 
 print(date[0])
+
 @bot.message_handler(commands=['start'])
 def start(message):
 	send_mess = req.TelegramInformation() + "\n" + f"<b>Привет {message.from_user.first_name} </b>! \n напиши сюда числа в формате ХХХХ-ХХ-ХХ, например 2020-07-24 мы знаем погоду в дате от {str(date[0])} до {str(date[len(date)-1])} "
@@ -16,7 +15,7 @@ def start(message):
 @bot.message_handler(content_types=['text'])
 def text(message):
     if message.text in date: 
-        for received_date in range(len(date)-1):
+        for received_date in range(len(date)):
             if(message.text.replace(' ', '') == date[received_date]):
                 bot.send_message(message.chat.id, req.weather_str[received_date])
     else:
