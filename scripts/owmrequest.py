f13af5fb08543d3e56620bbd13434b7e8403c1db
andrew <pixtergoood@gmail.com>
2020-08-14 21:27:02 +0500

    pep8
    переписал код по pep8

--- scripts/owmrequest.py
+++ scripts/owmrequest.py
@@ -1,23 +1,31 @@
 import requests
 
+
 def get_string_telegram(JsonFile):  # декоратор возврата строки из апи функции
     def api_string_for_telegram(*args, **kwargs):
         weather = []
         date = JsonFile()
         for i in date['list']:
-            weather.append(str(i['dt_txt'][:16]) +" " + '\n' + "Влажность " + str(i['main']['humidity']) + "%" + '\n'
-            + "Давление " +str(i['main']['pressure']) + " мм рт. ст. " + '\n' + "Температура " + "+" + str(round(i['main']['temp'])) + " " + '\n'
-            +"Скорость ветра " + str(i['wind']['speed']) + " " + "м/c"
-            + " " + str(get_wind_direction(i['wind']['deg'])) + '\n'
-            + str(i['weather'][0]['description']) + '\n' + "Вероятность осадков "
-            + str(i['pop']) + "%")
+            weather.append(str(i['dt_txt'][:16]) + " " + '\n' + "Влажность " + str(i['main']['humidity']) + "%" + '\n'
+                           + "Давление " + str(i['main']['pressure']) + " мм рт. ст. " + '\n' +
+                           "Температура " + "+" +
+                           str(round(i['main']['temp'])) + " " + '\n'
+                           + "Скорость ветра " +
+                           str(i['wind']['speed']) + " " + "м/c"
+                           + " " +
+                           str(get_wind_direction(i['wind']['deg'])) + '\n'
+                           + str(i['weather'][0]['description']) +
+                           '\n' + "Вероятность осадков "
+                           + str(i['pop']) + "%")
         return weather
     return api_string_for_telegram
 
 # Определяет с какой стороны летит ветер
+
+
 def get_wind_direction(deg) -> str:
-    l = ['С ','СВ',' В','ЮВ','Ю ','ЮЗ',' З','СЗ']
-    for i in range(0,8):
+    l = ['С ', 'СВ', ' В', 'ЮВ', 'Ю ', 'ЮЗ', ' З', 'СЗ']
+    for i in range(0, 8):
         step = 45.
         min = i*step - 45/2.
         max = i*step + 45/2.
@@ -29,10 +37,12 @@ def get_wind_direction(deg) -> str:
     return res
 
 # Проверка наличия в базе информации о нужном населенном пункте (опциональное использование)
+
+
 def get_city_id(s_city_name) -> int:
     try:
         res = requests.get("http://api.openweathermap.org/data/2.5/find",
-                     params={'q': s_city_name, 'type': 'like', 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+                           params={'q': s_city_name, 'type': 'like', 'units': 'metric', 'lang': 'ru', 'APPID': appid})
         data = res.json()
         cities = ["{} ({})".format(d['name'], d['sys']['country'])
                   for d in data['list']]
@@ -46,10 +56,12 @@ def get_city_id(s_city_name) -> int:
     return city_id
 
 # Запрос текущей погоды (вывод в консоль для дебага)
-def request_current_weather(city_id)-> None:
+
+
+def request_current_weather(city_id) -> None:
     try:
         res = requests.get("http://api.openweathermap.org/data/2.5/weather",
-                     params={'id': city_id, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+                           params={'id': city_id, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
         data = res.json()
         print("conditions:", data['weather'][0]['description'])
         print("temp:", data['main']['temp'])
@@ -61,13 +73,17 @@ def request_current_weather(city_id)-> None:
         pass
 
 # Прогноз
+
+
 def TelegramInformation(city_id=468902) -> str:
     res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
-                           params={'id': city_id, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+                       params={'id': city_id, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
     data = res.json()
-    return 'city: '+ data['city']['name'] + "\n" + data['city']['country']
+    return 'city: ' + data['city']['name'] + "\n" + data['city']['country']
 
 # получение json в функции и обработка строки json в декораторе
+
+
 @get_string_telegram
 def request_forecast(city_id=468902) -> str:
     try:
@@ -80,7 +96,8 @@ def request_forecast(city_id=468902) -> str:
         data = None
     return data
 
-#city_id for Yaroslavl
+
+# city_id for Yaroslavl
 city_id = 468902
 appid = "dca30b363934f02b817e916cb61ab3f3"   # OpenWeatherMap API
 
--- scripts/telegramSend.py
+++ scripts/telegramSend.py
@@ -6,10 +6,13 @@ date = [i.split(" ")[0] for i in req.weather_str]
 
 print(date[0])
 
+
 @bot.message_handler(commands=['start'])
 def start(message):
-	send_mess = req.TelegramInformation() + "\n" + f"<b>Привет {message.from_user.first_name} </b>! \n напиши сюда числа в формате ХХХХ-ХХ-ХХ, например 2020-07-24 мы знаем погоду в дате от {str(date[0])} до {str(date[len(date)-1])} "
-	bot.send_message(message.chat.id, send_mess, parse_mode = 'html')
+    send_mess = req.TelegramInformation() + "\n" + \
+        f"<b>Привет {message.from_user.first_name} </b>! \n напиши сюда числа в формате ХХХХ-ХХ-ХХ, например 2020-07-24"
+    "мы знаем погоду в дате от {str(date[0])} до {str(date[len(date)-1])} "
+    bot.send_message(message.chat.id, send_mess, parse_mode='html')
 
 
 @bot.message_handler(content_types=['text'])
@@ -17,7 +20,13 @@ def text(message):
     if message.text in date:
         for received_date in range(len(date)):
             if(message.text.replace(' ', '') == date[received_date]):
-                bot.send_message(message.chat.id, req.weather_str[received_date])
+                bot.send_message(
+                    message.chat.id, req.weather_str[received_date])
     else:
-        bot.send_message(message.chat.id, f"напиши сюда числа в формате ХХХХ-ХХ-ХХ, например 2020-07-24 я могу  показать прогноз погоды с {date[0]} до {str(date[len(date)-1])}")
+        bot.send_message(
+            message.chat.id, f"напиши сюда числа в формате ХХХХ-ХХ-ХХ, например 2020-07-24
+            "я могу  показать прогноз погоды с {date[0]} до {str(date[len(date)-1])}")
+
+
 bot.polling(none_stop=True)
+#
